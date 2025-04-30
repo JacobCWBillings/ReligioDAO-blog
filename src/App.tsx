5dd30e057dcae68c992674c4b211c09a9563a1c4
Jacob <billings.jacob@gmail.com>
2025-04-30 08:04:01 +0200

    Okay. Looking like progress.

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { Layout } from './components/Layout';
 
 // Pages
 import { HomePage } from './pages/HomePage';
-import { BlogEditorPage } from './pages/BlogEditorPage';
+import { EditorPage } from './EditorPage';
 import { BlogListPage } from './pages/viewer/BlogListPage';
 import { BlogDetailPage } from './pages/viewer/BlogDetailPage';
 import { GlobalSettingsPage } from './pages/GlobalSettingsPage';
@@ -182,47 +182,49 @@ export function App() {
             {/* Wrap application with enhanced WalletProvider with supported chain IDs */}
             <WalletProvider supportedChainIds={SUPPORTED_CHAIN_IDS}>
                 <GlobalStateProvider initialState={globalState} setGlobalState={setGlobalState}>
-                    <Routes>
-                        <Route
-                            path="/"
-                            element={
-                                <Layout 
-                                    isBeeRunning={isBeeRunning} 
-                                    hasPostageStamp={hasPostageStamp} 
-                                />
-                            }
-                        >
-                            {/* Home Page */}
-                            <Route index element={<HomePage />} />
-                            
-                            {/* Blog Editor Routes - Entry point for all blog creation */}
-                            <Route path="editor">
-                                <Route index element={<BlogEditorPage />} />
-                                <Route path=":blogId" element={<BlogEditorPage />} />
-                            </Route>
-                            
-                            {/* Blog Viewer Routes - Only for approved/minted blogs */}
-                            <Route path="blogs">
-                                <Route index element={<BlogListPage />} />
-                                <Route path=":blogId" element={<BlogDetailPage />} />
-                            </Route>
-                            
-                            {/* Proposal Routes - For both viewing and voting */}
-                            <Route path="proposals">
-                                <Route index element={<ProposalListPage />} />
-                                <Route path=":proposalId" element={<ProposalDetailPage />} />
-                            </Route>
-                            
-                            {/* Proposal Submission - Only accessible after editor */}
-                            <Route path="submit-proposal" element={<ProposalSubmissionPage />} />
-                            
-                            {/* Settings */}
-                            <Route path="settings" element={<GlobalSettingsPage />} />
-                            
-                            {/* 404 Fallback */}
-                            <Route path="*" element={<Navigate to="/" replace />} />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Layout 
+                                isBeeRunning={isBeeRunning} 
+                                hasPostageStamp={hasPostageStamp} 
+                            />
+                        }
+                    >
+                        {/* Home Page */}
+                        <Route index element={<HomePage />} />
+                        
+                        {/* Editor Routes - Consolidated */}
+                        <Route path="editor">
+                            <Route index element={<EditorPage mode="standard" />} />
+                            <Route path=":blogId" element={<EditorPage mode="standard" />} />
                         </Route>
-                    </Routes>
+                        
+                        {/* Proposal Editor Route */}
+                        <Route path="proposal-editor" element={<EditorPage mode="proposal" />} />
+                        
+                        {/* Blog Viewer Routes */}
+                        <Route path="blogs">
+                            <Route index element={<BlogListPage />} />
+                            <Route path=":blogId" element={<BlogDetailPage />} />
+                        </Route>
+                        
+                        {/* Proposal Routes */}
+                        <Route path="proposals">
+                            <Route index element={<ProposalListPage />} />
+                            <Route path=":proposalId" element={<ProposalDetailPage />} />
+                        </Route>
+                        <Route path="submit-proposal" element={<ProposalSubmissionPage />} />
+                        
+                        {/* Settings */}
+                        <Route path="settings" element={<GlobalSettingsPage />} />
+                        
+                        {/* 404 Fallback */}
+                        <Route path="*" element={<Navigate to="/" replace />} />
+                    </Route>
+                </Routes>
+
                 </GlobalStateProvider>
             </WalletProvider>
         </BrowserRouter>
